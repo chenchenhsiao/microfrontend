4fe76de19ce4075cb4efbc55f6d77cb8a54363f2
Chenchen Hsiao <chsiao@costargroup.com>
2025-07-15 15:26:08 -0400

    updated container.yml

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -10,7 +10,7 @@ import MarketingApp from "./components/MarketingApp"; // moved mount into Market
 export default () => {
   return (
     <>
-      <h1>Hi there!</h1>
+      <h1>Hi there!!!</h1>
       <hr />
       <MarketingApp />
     </>
